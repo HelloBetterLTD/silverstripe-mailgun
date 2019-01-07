ffefae8aec2709062f8f45b59c6d985538008dc4
Pramesh Harshana <pramesh@silverstripers.com>
2019-01-07 10:35:44 +0530

    build a basic class for mailgun integration

--- silverstripemailgun/README.md
+++ silverstripemailgun/README.md
@@ -1,2 +1,16 @@
 # silverstripe-mailgun
 Mailgun integration for SilverStripe
+
+Create a configfile with following contents
+
+```yaml
+
+---
+Name: silverstripemailgun
+---
+SilverStripers\silverstripemailgun\Models\SilverStripersMailer:
+  SMTPusername: 'Your mailgun SMTPLogin'
+  SMTPpassword: 'Your mailgun password'
+  
+  
+```
--- silverstripemailgun/src/Models/SilverStripersMailer.php
+++ silverstripemailgun/src/Models/SilverStripersMailer.php
@@ -1,7 +1,6 @@
 <?php
 namespace SilverStripers\silverstripemailgun\Models;
-use SilverStripe\Control\Email\SwiftMailer;
-
+use SilverStripe\Control\Email\SwiftMailer as MainSwiftMailer;
 /**
  * Created by PhpStorm.
  * User: mpowerPC
@@ -9,24 +8,27 @@ use SilverStripe\Control\Email\SwiftMailer;
  * Time: 10:52 AM
  */
 
-class SilverStripersMailer extends SwiftMailer
+class SilverStripersMailer extends MainSwiftMailer
 {
     protected $mailgunClient;
 
     public function __construct()
     {
         $config = $this->config();
+        // create the Transport
         $transport = \Swift_SmtpTransport::newInstance('smtp.mailgun.org', 25)
             ->setUsername($config->SMTPusername)
             ->setPassword($config->SMTPpassword);
+        // create the Mailer using your created Transport
         $client = \Swift_Mailer::newInstance($transport);
         $this->setMailGunClient($client);
     }
 
-    //@from array of email address with show nAME
-    //@$to array of email addresses
-    //@$subject string
-    //@$content text
+    //@$to array of email addresses of reciever
+    //@from array of email address with show nAME of sender
+    //@$subject string ,subject of email
+    //@$content text , plain content of email
+    //@$attachments array of file links, this could be set of urls or file paths on the disk
     public function sendPlain($to = [], $from = [], $subject = '', $content = '', $attachments = [])
     {
 
@@ -44,6 +46,12 @@ class SilverStripersMailer extends SwiftMailer
         return $this->sendMessage($message);
     }
 
+    //@$to array of email addresses of reciever
+    //@from array of email address with show nAME of sender
+    //@$subject string ,subject of email
+    //@$plainContent text , plain content of email
+    //@$htmlContent text , HTML Text content of email
+    //@$attachments array of file links, this could be set of urls or file paths on the disk
     public function sendHTML($to, $from, $subject, $htmlContent, $plainContent = '', $attachments = [])
     {
         $message = \Swift_Message::newInstance()
@@ -60,17 +68,23 @@ class SilverStripersMailer extends SwiftMailer
         return $this->sendMessage($message);
     }
 
+    //send the email
+    //returns boolean
     protected function sendMessage($message)
     {
         return $this->mailgunClient->send($message);
     }
 
+    //set the mailgunClient
+    //returns this
     public function  setMailGunClient($client)
     {
         $this->mailgunClient = $client;
         return $this;
     }
 
+    //get the mailgunClient
+    //returns Swift_Mailer object
     public function getMailgunClient()
     {
         return $this->mailgunClient;
